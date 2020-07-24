0c561a45a7d7c1b608c235f754fbbd9340f7252b
esctrionsit <esctrionsit@hotmail.com>
2020-07-24 18:58:27 +0800

    bug fix

--- runtime_scripts/newstring.R
+++ runtime_scripts/newstring.R
@@ -35,7 +35,7 @@ ns_readdata <- function(ty1, ty2, samp, ns_ge, ro){
             if(ty1 != "both"){
                 shell <- paste(shell, " -e \'TYPE!=\"", ty1, "\"\' ", sep = "")
             }
-            if(ty2 != "all") { shell <- paste(shell, " -e \'GT==\"het\"\' ", sep = "") } 
+            if(ty2 != "all") { shell <- paste(shell, " -H R ", sep = "") } 
             shell <- paste(shell, " |", path_seqkit, " seq", sep = "")
         }
         #fra_ns_finalres <<- rbind(fra_ns_finalres, data.frame(c(shell)))
